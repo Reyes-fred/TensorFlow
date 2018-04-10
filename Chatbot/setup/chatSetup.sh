bbad58e3956d595683fbd97469c1bb657f392813
Alfredo Reyes <reyes-fred@hotmail.com>
2018-04-10 15:44:08 -0700

    Update

---
+++ Chatbot/setup/chatSetup.sh
@@ -0,0 +1,13 @@
+echo
+echo "Welcome to the magic world of TensorFLow"
+echo 
+
+python3 -m pip install tensorflow
+python3 -m pip install regex
+python3 -m pip install tdqm
+python3 -m pip isntall colorama
+python3 -m pip install python-levenshtein
+
+echo 
+echo "Finish"
+echo
